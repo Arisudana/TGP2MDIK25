4beeb6d90442636065e8b04a94775cfaebfebe3b
NikolausVico <100015538+NikolausVico@users.noreply.github.com>
2025-07-01 22:30:57 +0700

    Ingestion

---
+++ spark_ingest_sales.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+
+# Inisialisasi SparkSession
+spark = SparkSession.builder.appName("IngestCSVtoHDFS").getOrCreate()
+
+# Langkah 1: Baca dari filesystem lokal (bisa juga gunakan path relatif/absolut)
+df = spark.read.option("header", True).csv("C:/Users/Nikolaus/Desktop/fp_midk/Superstore.csv")
+
+# (Opsional) Bisa tambahkan transformasi di sini misalnya cleaning
+# df = df.withColumn(...)
+
+# Langkah 2: Simpan langsung ke HDFS
+df.write.mode("overwrite").option("header", True).csv("hdfs://localhost:9000/user/data/Superstore.csv")
+
+# Selesai
+spark.stop()
+print("✅ File berhasil di-ingest ke HDFS.")
